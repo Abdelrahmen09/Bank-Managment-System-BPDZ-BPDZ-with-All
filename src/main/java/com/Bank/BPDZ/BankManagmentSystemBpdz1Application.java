c34f4528afeed319d48128121b599dad3392c1df
Abdelrahmen09 <omarmansouri20001@gmail.com>
2025-05-24 14:10:34 +0200

    change it it

--- src/main/java/com/Bank/BPDZ/BankManagmentSystemBpdz1Application.java
+++ src/main/java/com/Bank/BPDZ/BankManagmentSystemBpdz1Application.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.Bank.BPDZ.Entity.BPDZCptBcDCA;
@@ -18,7 +19,7 @@ import com.Bank.BPDZ.Entity.BPDZHabi;
 import com.Bank.BPDZ.Repository.RepositoryBpdzDir;
 import com.Bank.BPDZ.Repository.RepositoryBpdzHbi;
 import com.Bank.BPDZ.Service.ServiceHabi;
-
+ @EnableJpaRepositories(basePackages = "com.Bank.BPDZ.Repository")
 @SpringBootApplication
 public class BankManagmentSystemBpdz1Application implements CommandLineRunner{
 	
